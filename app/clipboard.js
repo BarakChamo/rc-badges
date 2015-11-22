4b0fa0e3c968092be159995a0a44263b004296f0
Barak Chamo <barakchamo@gmail.com>
2015-11-22 15:18:14 +0200

    doge

--- app/clipboard.js
+++ app/clipboard.js
@@ -1,6 +1,8 @@
 const textArea = document.createElement('textarea')
 document.body.appendChild(textArea)
 
+let timeout = null
+
 const clipboard = {
 	copy: text => {
 		// Clear all selection
@@ -15,6 +17,12 @@ const clipboard = {
 
 		// Copy to clipboard
 		document.execCommand('copy')
+
+		document.body.classList.add('copied')
+
+		if (timeout) clearTimeout(timeout)
+
+		timeout = setTimeout(ts => document.body.classList.remove('copied'), 3000)
 	}
 }
 
